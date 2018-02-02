04c7bceb8d3005d60d2d8c014bb314d7cdccb0d1
Peter Lane <muffinspawn@gmail.com>
2018-02-02 11:00:49 -0600

    Moved pygen.py to new bin/ directory. Added batch and bash scripts for calling pygen.py with the appropriate arguments.

---
+++ bin/generate-packet-python.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+python ${SCRIPT_DIR}/pygen.py ${SCRIPT_DIR}/../Include/ES_C_API_Def_clean.h > ${SCRIPT_DIR}/../CESAPI/packet.py

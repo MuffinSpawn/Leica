eb8352b40c4b73ac28f7727e531a94675e706bc9
Peter Lane <muffinspawn@gmail.com>
2018-02-23 22:21:06 -0600

    added license stuff

--- setup.py
+++ setup.py
@@ -35,6 +35,8 @@ setup(
     # https://packaging.python.org/en/latest/single_source_version.html
     version='1.1.0beta3',  # Required
 
+    license='MIT License',
+
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
@@ -143,6 +145,7 @@ setup(
     #
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
     # data_files=[('my_data', ['data/data_file'])],  # Optional
+    data_files = [("", ["LICENSE"])]
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
